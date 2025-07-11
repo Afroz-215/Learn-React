b7bcff60c78f7885d9266deb617549a28e875485
Afroz-215 <afrozj215@gmail.com>
2025-07-11 20:51:53 +0530

    Learning react

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  let arr = [1, 2, 3, 4, 5, 6]
+
+  function fun1(){
+    console.log("first")
+  }
+  return (
+    <>
+    <button onClick={fun1}>click</button>
+    <div>App
+      {
+        arr.map((a, b, c) => {
+          return (<>
+          <h1>{a}</h1>
+          </>)
+        })
+      }
+    </div>
+    </>
+    
+    
+  )
+}
+
+export default App
