dd1fc7479be96582234ab98b7f8887989a95942d
Afroz-215 <afrozj215@gmail.com>
2025-07-25 20:44:03 +0530

    updated useEffect

--- my-react-app/src/Day5.jsx
+++ my-react-app/src/Day5.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
+import './Day5.css'
 
 const Day5 = () => {
-    let [count, SetCount] = useState(0);
+    // let [count, SetCount] = useState(0);
     let [input, SetInput] = useState([]);
 
     useEffect(() => {
@@ -12,26 +13,37 @@ const Day5 = () => {
 
             }).then((data) => {
                 SetInput(data)
-                console.log(input);
+                console.log(data);
 
             })
 
     }, [])
 
+    function d(index){
+        let newArr =input.filter((a)=>{
+            return a.id !== index
+        })
+         SetInput(newArr);
+    }
+   
+
 
     return (
         <div>
-            <button onClick={() => SetCount(count + 1)}> count {count}</button>
+            {/* <button onClick={() => SetCount(count + 1)}> count {count}</button> */}
+            <div id='parent_Card'>
 
-            {
-                input.map((a) => {
-                    return (<>
-                        <h2>{a.id}</h2>
-                        <h1>{a.title}</h1>
+                {
+                    input.map((a) => {
+                        return (<div id='card' onClick={()=>d(a.id)}>
+                            <h2>{a.id}</h2>
+                            <h1>{a.title}</h1>
+                            <button>add</button>
 
-                    </>)
-                })
-            }
+                        </div>)
+                    })
+                }
+            </div>
         </div>
     )
 }
