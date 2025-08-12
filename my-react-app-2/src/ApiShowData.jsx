4c7912d38e2a3e82a41ec02fb8fe60334c880ee3
Afroz-215 <afrozj215@gmail.com>
2025-08-12 23:02:40 +0530

    useContext

--- my-react-app-2/src/ApiShowData.jsx
+++ my-react-app-2/src/ApiShowData.jsx
@@ -12,18 +12,20 @@ const ApiShowData = () => {
     </Link>
      
     <div id='parent_Card'>  
-
+      
     {
         state.data.map((a,index)=>{
+                let inCart=  state.cartData.find(item=>item.id===a.id)
+         console.log(inCart,"isko dekhooo");
             return(<div id='card'  >
                 <img  src={a.image}/>
                 <p>{a.name}</p>
-                <button onClick={()=>{dispatch({type:"addToCart",payload:a})}}>click</button>
-
-
-                
+                {
+                  inCart?(<div> <button  onClick={()=>dispatch({type:"increment",payload:a.id})}>++</button> {inCart.
+                    quantity
+                    }  <button onClick={()=>dispatch({type:"decrement",payload:a.id})}>--</button> </div>):( <button onClick={()=>{dispatch({type:"addToCart",payload:a})}}>click</button> )
+                }
                
-              
             </div>)
         })
     }
